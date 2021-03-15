db2a6ce36899193b096df39436a00e7b8e089246
kirjai <kirjaitube@gmail.com>
2021-03-15 14:41:17 +0200

    .

--- apps/next-app/next.config.js
+++ apps/next-app/next.config.js
@@ -1,6 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withNx = require('@nrwl/next/plugins/with-nx');
 
+console.log('ENV', process.env);
+
 module.exports = withNx({
   trailingSlash: true,
   reactStrictMode: true,
